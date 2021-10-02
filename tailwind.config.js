71b58d7ad076cc7771e4b3cd6d528e8d02631a6e
Jason Aravanis <j.aravanis@icloud.com>
2021-10-02 10:26:53 +1000

    Add AvenirNext font, custom bg color to tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        "background-color": "#eceef1",
+      },
+      fontFamily: {
+        avenir: "AvenirNext",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
